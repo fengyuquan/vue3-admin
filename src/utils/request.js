763ca594dbab6f43b080574f270a3ecacaa4da69
fengyuquan <420125423@qq.com>
2022-04-13 14:59:09 +0800

    feat: 为axios增加请求拦截，如果vuex存在token，则访问头需携带token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,4 @@
+import store from '@/store'
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 
@@ -6,17 +7,28 @@ const service = axios.create({
   timeout: 5000
 })
 
+// 请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    const token = store.getters.token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config // 必须返回配置
+  },
+  (error) => {
+    Promise.reject(error)
+  }
+)
+
 // 响应拦截器
 service.interceptors.response.use(
   (response) => {
     const { data } = response
-    const { message } = data
-
-    if (message) ElMessage.success(message)
     return data ?? ''
   },
-  (err) => {
-    const { data } = err.response
+  (error) => {
+    const { data } = error.response
     const { message } = data
 
     if (message) ElMessage.error(message)
